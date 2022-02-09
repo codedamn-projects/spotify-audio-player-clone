6f55bb3b038497a0012d6fdfabadf3bbf009e10c
Pranav <pranav.techiegeek@gmail.com>
2022-02-10 00:10:26 +0530

    basic readme ready

---
+++ songs.js
@@ -0,0 +1,34 @@
+const songs = [
+  {
+    name: "Come 2gether",
+    duration: 222,
+    artist: "Ooyy",
+    album: "Come 2gether",
+  },
+  {
+    name: "Genesis",
+    duration: 222,
+    artist: "Ooyy",
+    album: "Genesis",
+  },
+  {
+    name: "Flames",
+    duration: 222,
+    artist: "Ooyy",
+    album: "Flames",
+  },
+  {
+    name: "Calling Me",
+    duration: 222,
+    artist: "Ooyy",
+    album: "Calling Me",
+  },
+  {
+    name: "Thunderbird",
+    duration: 222,
+    artist: "Ooyy",
+    album: "Thunderbird",
+  },
+];
+
+export default songs;
